5a1b5190e71ccdf5a4d103b8a49c03d132fbcf97
MaxRuxes <terasobaka@yandex.ru>
2018-03-12 00:29:36 +0300

    Added  view and viewmodel for authorzation window

--- TaskList/ToolKit/ViewModel/AppBootstrapper.cs
+++ TaskList/ToolKit/ViewModel/AppBootstrapper.cs
@@ -18,7 +18,7 @@ namespace TaskList.ToolKit.ViewModels
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
-            DisplayRootViewFor<MainWindowViewModel>();
+            DisplayRootViewFor<AuthorizationWindowViewModel>();
         }
     }
 }
